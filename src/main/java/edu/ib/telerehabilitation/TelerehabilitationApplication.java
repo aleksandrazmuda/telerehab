d387c02d1d1c91618400f211cfb90a2861c52581
Aleksandra <aleksandrazmuda1998@gmail.com>
2020-11-11 21:18:26 +0100

    initial commit

---
+++ src/main/java/edu/ib/telerehabilitation/TelerehabilitationApplication.java
@@ -0,0 +1,14 @@
+package edu.ib.telerehabilitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelerehabilitationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelerehabilitationApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
