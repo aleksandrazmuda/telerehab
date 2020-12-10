156ab325f6008ae7afad238fda7ce3365fb47e5e
Aleksandra <aleksandrazmuda1998@gmail.com>
2020-12-10 20:43:38 +0100

    register and log in forms added

--- src/main/java/edu/ib/telerehabilitation/controller/RegistrationController.java
+++ src/main/java/edu/ib/telerehabilitation/controller/RegistrationController.java
@@ -4,23 +4,23 @@ import edu.ib.telerehabilitation.dao.Specialist;
 import edu.ib.telerehabilitation.service.SpecialistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.List;
-
 @Controller
-public class SpecialistController {
+public class RegistrationController {
 
     private SpecialistService specialistService;
 
     @Autowired
-    public SpecialistController(SpecialistService specialistService) {
+    public RegistrationController(SpecialistService specialistService) {
         this.specialistService = specialistService;
     }
 
-
+    @GetMapping("/registration")
+    public String goToReg() {
+        return "registration";
+    }
 }
