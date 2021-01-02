806f2e946585f7f7cd23c7600127c7f56b369783
Aleksandra <aleksandrazmuda1998@gmail.com>
2021-01-02 02:22:39 +0100

    config webrtc changed

--- src/main/java/edu/ib/telerehabilitation/webrtcConfig/WebSocketConfiguration.java
+++ src/main/java/edu/ib/telerehabilitation/webrtcConfig/WebSocketConfiguration.java
@@ -1,4 +1,4 @@
-package edu.ib.telerehabilitation.webrtcConfig;//package edu.ib.telerehabilitation.webrtc;
+//package edu.ib.telerehabilitation.webrtcConfig;
 //
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.web.socket.config.annotation.EnableWebSocket;
